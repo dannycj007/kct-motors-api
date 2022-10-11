654f4bb83c08101008c8f1190176a4a98a9ca12d
dannycj007 <dannyvoncharles007@gmail.com>
2022-10-11 11:02:03 +0300

    api updates

---
+++ src/api/chasis-number/routes/chasis-number.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * chasis-number router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::chasis-number.chasis-number');
---
+++ src/api/chasis-number/services/chasis-number.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * chasis-number service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::chasis-number.chasis-number');

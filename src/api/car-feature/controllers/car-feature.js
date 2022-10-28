612c92554ada404a09297c507898ae6a27fd75de
dannycj007 <dannyvoncharles007@gmail.com>
2022-10-28 19:36:01 +0300

    Current Strapi Updates

---
+++ src/api/car-feature/controllers/car-feature.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * car-feature controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::car-feature.car-feature');
---
+++ src/api/car-feature/routes/car-feature.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * car-feature router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::car-feature.car-feature');
